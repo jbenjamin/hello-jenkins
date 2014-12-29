30bf212fddabcbe9d9048d91801ffb11dc1b5318
Benjamin Aguilar <benjamin.aguilar@creativaconsultores.com>
2014-12-29 15:24:14 -0600

    Switch test to hello jenkins

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var request = require('supertest');
 var app = require('../app.js');
  
 describe('GET /', function() {
-  it('respond with hello world from my first Node App!!.. J', function(done) {
-    request(app).get('/').expect('hello world from my first Node App!!.. J', done);
+  it('respond with hello Jenkins!!.. J', function(done) {
+    request(app).get('/').expect('hello Jenkins!!.. J', done);
   });
 });
